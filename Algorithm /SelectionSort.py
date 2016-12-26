92dd9e955e372a76494770b6d3fc513c55ab7d07
Nishchith K <inishchith@gmail.com>
2016-12-26 10:14:11 +0530

    Update and rename 2.SelectionSort.py to SelectionSort.py

--- Algorithm /SelectionSort.py
+++ Algorithm /SelectionSort.py
@@ -3,12 +3,10 @@ x=len(a)
 # Traverse through all array elements
 for i in range(x):
     min_=i
-    # Find the minimum element in remaining
-    # unsorted array
+    # Find the minimum element in remaining unsorted array
     for j in range(i+1,x):
         if a[min_]>a[j]:
             min_=j
-    # Swap the found minimum element with
-    # the first element
+    # Swap the found minimum element with the first element
     a[i],a[min_]=a[min_],a[i]
 print(a)
