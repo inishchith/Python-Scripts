a45cb93eace33646f8eb50beeceff80f02384cf1
inishchith <inishchith@gmail.com>
2017-02-04 19:02:51 +0530

    Cleaned

--- Algorithm /InsertionSort.py
+++
@@ -1,9 +0,0 @@
-arr=list(map(int,input().split()))
-for i in range(1,len(arr)):
-    key=arr[i]
-    j=i-1
-    while j>=0 and arr[j]>key:
-        arr[j+1]=arr[j]
-        j-=1
-    arr[j+1]=key
-print(' '.join(map(str,arr)))
