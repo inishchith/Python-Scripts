cf02a7ce2241656138b2112d469f74e1f3f308a0
inishchith <inishchith@gmail.com>
2017-05-22 18:51:55 +0530

    .travis.yml

---
+++ Shell.py
@@ -0,0 +1,39 @@
+import sys , os
+import shlex
+
+#support
+
+commands = []
+
+
+def tokenize(command):
+    return shlex.split(command)
+
+def shell():
+    status = True
+    while status == True :
+        sys.stdout.write('> ')
+        sys.stdout.flush()
+        string = sys.stdin.readline()
+        commands = tokenize(string)
+        status = execute(commands)
+
+def execute(commands) :
+
+    # child process check
+    processid = os.fork()
+
+    if processid == 0:
+        os.execvp(commands[0],commands)
+    else:
+        #parent
+        while True :
+            wpid , status = os.waitpid(processid,0)
+
+            if os.WIFEXITED(status)  or os.WIFSIGNALED(status):
+                break
+    return True
+
+if __name__ == '__main__' :
+    print("> Welcome to Shell")
+    shell()
